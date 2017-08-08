0518a53ab514ddb38b9a8b29cb313ba769c2a55f
Chih-Hao <zhihaozhang@me.com>
2017-08-08 14:15:10 +0800

    Initial Commit

---
+++ 新氦Pass/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  新氦Pass
+//
+//  Created by Chih-Hao on 2017/8/8.
+//  Copyright © 2017年 Chih-Hao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
